2afc1b5db9a6c11458bcc97df5a08e7c9d323910
Ezeikel Pemberton <ezeikelpemberton@gmail.com>
2017-06-10 03:04:06 +0100

    Cleaned up code in houses view.

--- js/views/houses.js
+++ js/views/houses.js
@@ -10,30 +10,26 @@ define([
 
     var HousesView = Backbone.View.extend({
       initialize: function() {
-        _.bindAll(this, 'render');
 
-        // Create a collection
         this.collection = new Houses();
 
-        // Fetch the collection and call render() method
         var that = this;
         this.collection.fetch({
           data: {
             pageSize: 50
           },
-          success: function (collection, response, options) {
+          success: function () {
             that.render();
           },
-          error: function (collection, response, options) {
-            console.log('There was an error in fetch.');
+          error: function () {
+            console.error('There was an error in fetch');
           }
         });
       },
-      tagName: 'section',
       template: Handlebars.getTemplate('houses'),
       render: function() {
         this.$el.html(this.template({ houses : this.collection.toJSON() }));
-        //debugger;
+
         return this;
       }
     });
